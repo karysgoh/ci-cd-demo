00081bb0fb6ee35a076fae38e7fe35ee1904e4ec
Karys <k4rysgoh@gmail.com>
2025-11-03 15:13:34 +0800

    Update CI/CD workflow to build web application from the correct directory and adjust Vite server port

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react()] as any,
     server: {
-        port: 5173,
+        port: 5175,
         host: true,
         proxy: {
             '/api': {
